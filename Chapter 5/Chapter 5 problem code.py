78582a705cb0dcdcc485f7577df73b58a59b5709
Thomas Abraham <Thomas.Abraham@octoconsulting.com>
2022-08-02 09:24:22 -0400

    chapter 5 update

--- Chapter 5/Chapter 5 problem code.py
+++ Chapter 5/Chapter 5 problem code.py
@@ -90,7 +90,7 @@ if numbers <= numberCompare:
 elif numbers >= numberCompare:
     print(str(numbers) + " is greater than/equal " + str(numberCompare))
 
-if numbers <= numberCompare and numbers >= 20:
+if numberCompare >= numbers >= 20:
     print(True)
 else:
     print(False)
@@ -138,6 +138,136 @@ else:
 
 print("Your admission cost is $" + str(price))
 
+toppings = ['mushrooms', 'extra cheese', 'pepperoni', 'beef']
+if 'mushroom' in toppings:
+    print('Adding mushrooms.')
+if 'extra cheese' in toppings:
+    print('Adding extra cheese')
+if 'pepperoni' in toppings:
+    print('Adding pepperoni')
 
+print('\nFinished making your pizza.')
+
+# 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a
+# variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+# •	 Write an if statement to test whether the alien’s color is green. If it is, print
+# a message that the player just earned 5 points.
+# •	 Write one version of this program that passes the if test and another that
+# fails. (The version that fails will have no output.)
+alienColor = 'red'
+if alienColor == 'green':
+    print('player has earned 5 points')
+# 5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and
+# write an if-else chain.
+# •	 If the alien’s color is green, print a statement that the player just earned
+# 5 points for shooting the alien.
+# •	 If the alien’s color isn’t green, print a statement that the player just earned
+# 10 points.
+# •	 Write one version of this program that runs the if block and another that
+# runs the else block.
+alienColor = 'red'
+if alienColor == 'green':
+    print('player has earned 5 points')
+if alienColor != 'green':
+    print('player has earned 10 points.')
+
+alienColor = 'red'
+if alienColor == 'green':
+    print('player has earned 5 points')
+else:
+    print('player has earned 10 points')
+
+# 5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and
+# write an if-else chain.
+# •	 If the alien’s color is green, print a statement that the player just earned
+# 5 points for shooting the alien.
+# •	 If the alien’s color isn’t green, print a statement that the player just earned
+# 10 points.
+# •	 Write one version of this program that runs the if block and another that
+# runs the else block.
+alienColor = 'red'
+if alienColor == 'green':
+    print('Player has earned 5 points.')
+elif alienColor == 'yellow':
+    print('Player has earned 10 points.')
+else:
+    print('Player has earned 15 points.')
+
+# 5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+# •	 If the alien is green, print a message that the player earned 5 points.
+# •	 If the alien is yellow, print a message that the player earned 10 points.
+# •	 If the alien is red, print a message that the player earned 15 points.
+# •	 Write three versions of this program, making sure each message is printed
+# for the appropriate color alien.
+alienColor = 'red'
+if alienColor == 'green':
+    print('Player has earned 5 points.')
+elif alienColor == 'yellow':
+    print('Player has earned 10 points.')
+else:
+    print('Player has earned 15 points.')
+# 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s
+# stage of life. Set a value for the variable age, and then:
+# •	 If the person is less than 2 years old, print a message that the person is
+# a baby.
+# •	 If the person is at least 2 years old but less than 4, print a message that
+# the person is a toddler.
+# •	 If the person is at least 4 years old but less than 13, print a message that
+# the person is a kid.
+# •	 If the person is at least 13 years old but less than 20, print a message that
+# the person is a teenager.
+# •	 If the person is at least 20 years old but less than 65, print a message that
+# the person is an adult.
+# •	 If the person is age 65 or older, print a message that the person is an
+# elder.
+personAge = 67
+if personAge < 2:
+    print('\nPerson is a baby.')
+elif 2 < personAge < 4:
+    print('\nPerson is a toddler.')
+elif 4 < personAge < 13:
+    print('\nPerson is a kid.')
+elif 13 < personAge < 20:
+    print('\nPerson is a teenager.')
+elif 20 < personAge < 65:
+    print('\nPerson is an adult.')
+elif personAge >= 65:
+    print('\nPerson is an elder.')
+
+# 5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of
+# independent if statements that check for certain fruits in your list.
+# •	 Make a list of your three favorite fruits and call it favorite_fruits.
+# •	 Write five if statements. Each should check whether a certain kind of fruit
+# is in your list. If the fruit is in your list, the if block should print a statement,
+# such as You really like bananas!
+
+favFruits = ['watermelon', 'apple', 'orange']
+if 'watermelon' in favFruits:
+    print('You really like watermelons.')
+if 'apple' in favFruits:
+    print('You really like apples.')
+if 'orange' in favFruits:
+    print('You really like oranges.')
+if 'banana' in favFruits:
+    print('You really like bananas.')
+if 'pineapple' in favFruits:
+    print('You really like pineapples.')
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, out of green peppers.")
+    else:
+        print("\nAdding " + requested_topping + ".")
+print("\nFinished making your pizza!")
+
+availableToppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in availableToppings:
+        print("\nAdding " + requested_topping + ".")
+    else:
+        print("\nSorry, we dont have " + requested_topping + ".")
+print("\nFinished making your pizza!")
 
 

5e7bfdc058e67355aeabc24ae6be216f299cf480
Thomas Abraham <Thomas.Abraham@octoconsulting.com>
2022-08-08 12:24:13 -0400

    chapter 6 code

--- Chapter 6/Chapter 6.py
+++ Chapter 6/Chapter 6.py
@@ -123,7 +123,7 @@ for country in famousRivers.values():
 # •	 Make a list of people who should take the favorite languages poll. Include
 # some names that are already in the dictionary and some that are not.
 # •	 Loop through the list of people who should take the poll. If they have
-# already taken the poll, print a message thanking them for responding..
+# already taken the poll, print a message thanking them for responding.
 # If they have not yet taken the poll, print a message inviting them to take
 # the poll.
 

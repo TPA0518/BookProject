394fbc05e1dcfabd9cc14743f59348c0a8b65a34
TPA0518 <TAbraham611@gmail.com>
2022-08-08 12:21:23 -0400

    Chapter 5 code

--- Chapter 5/Chapter 5 problem code.py
+++ Chapter 5/Chapter 5 problem code.py
@@ -304,7 +304,7 @@ for nUser in newUsersLow:
 # •	 Store the numbers 1 through 9 in a list.
 # •	 Loop through the list.
 # •	 Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th
-# 7th 8th 9th", and each result should be on a separate line
+# 7th 8th 9th", and each result should be on a separate line.
 
 
 ordNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
